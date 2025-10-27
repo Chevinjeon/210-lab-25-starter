5326172c5453b9d327b259b842beb86029836717
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-10-26 21:54:00 -0700

    second commit

--- main.cpp
+++ main.cpp
@@ -7,7 +7,16 @@
 #include <iomanip>
 #include <fstream>
 using namespace std;
+using namespace std::chrono;
 
+const string DATA_FILE = "codes.txt";
+const string TEST_VALUE = "TESTCODE";
+const int NUM_ELEMENTS = 20000;
+
+// Read data into vector
+long long read_vector(vector<string>& vec) {
+    auto start = high_resolution_clock::now();
+    
 int main() {
     
 
